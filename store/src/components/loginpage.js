cc9c67ed47686affd379f4af2f6c5a8a4d0b53b3
HPKXKKG <HPKXKKG@3mhealth.com>
2025-05-13 16:25:40 +0530

    Login page

--- store/src/components/loginpage.js
+++ store/src/components/loginpage.js
@@ -2,14 +2,14 @@
 
 // LoginPage.jsx
 import React from 'react';
-import { Box, Grid, Paper, TextField, Button, Typography, useMediaQuery } from '@mui/material';
+import { Box, Grid, Paper, TextField, Button, Typography, useMediaQuery, Link, Card, CardMedia } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import bannerImage from '../assets/banner.jpg';
+import logo from '../assets/logo.png';
 
 const LoginPage = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
-
   return (
     <Box sx={{ flexGrow: 1 }}>
     <Grid container spacing={0}>
@@ -21,6 +21,20 @@ const LoginPage = () => {
           <Box  container sx={{ height: '100vh', alignItems: 'center',
           justifyContent: 'center', display: 'flex', p: "4", }}>
             <Box sx={{ maxWidth: 400, width: '100%'  }}>
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <img
+                  src={logo}
+                  alt="logo Image"
+                  width="150"
+                />
+                {/* <CardMedia
+                  component="img"
+                  width="50"
+                  image={logo}
+                  alt="logo Image"
+                /> */}
+              </Box>
+              
               <Typography variant="h5" align="center" gutterBottom>
                 Sign In
               </Typography>
@@ -29,6 +43,10 @@ const LoginPage = () => {
               <Button fullWidth variant="contained" sx={{ mt: 2 }}>
                 Login
               </Button>
+              <Box display="flex" gap={2} mt={2}>
+                <Link href="/link1">Forgot password?</Link>
+                <Link href="/link2">Create a New Account</Link>
+              </Box>
             </Box>
           </Box>
       </Grid>
